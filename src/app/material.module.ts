f5cc712165fa981a3f9da1fa0db2808de43b9f9e
thomasnrg <thomasnrgsen@gmail.com>
2018-02-09 12:23:53 -0400

    Construcción de pantalla de detalle de una pregunta

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core'
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon'
+import {MatCardModule} from '@angular/material/card';
+
 
 
 const modules = [
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule
 ]
 
 @NgModule ({
--- src/app/question/question-detail.componet.ts
+++ src/app/question/question-detail.componet.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import { Question } from './question.model'
 
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html'
 })
 
-export class QuestionDetailComponet {}
+export class QuestionDetailComponet {
+  question: Question = new Question(
+    'Esta es una nueva pregunta sobre IOS',
+    'Tengo una pregunta acerca de IOS...',
+    new Date,
+    'devicon-apple-original'
+  );
+}
