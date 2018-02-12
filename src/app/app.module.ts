b0a062930e9b9922a990b589c9604d9cc46709a9
thomasnrg <thomasnrgsen@gmail.com>
2018-02-12 13:15:42 -0400

    Crear la pantalla para cargar una nueva pregunta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AnswerFormComponent } from './answer/answer-form.componet';
 import { SigninScreenComponent }  from "./auth/signin-screen.componet";
 import { SignupScreenComponent }  from "./auth/signup-screen.component";
 import { QuestionListComponent } from './question/question-list.component';
+import { QuestionFormComponent } from './question/question-form.component'
 
 import { MomentModule } from 'angular2-moment';
 
@@ -24,7 +25,8 @@ import { MomentModule } from 'angular2-moment';
     AnswerFormComponent,
     SigninScreenComponent,
     SignupScreenComponent,
-    QuestionListComponent
+    QuestionListComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
