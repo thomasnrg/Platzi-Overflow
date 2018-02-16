5b4717d63034419c7b1e936aba5485ffdbc40d8d
thomasnrg <thomasnrgsen@gmail.com>
2018-02-15 21:32:27 -0400

    Creando el servicio de preguntas

---
+++ src/app/question/question.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core'
+import { Question } from './question.model'
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment'
+import urljoin from 'url-join'
+
+@Injectable()
+
+export class QuestionService {
+
+  private questionsUrl: string
+
+  constructor(private http: Http) {
+    this.questionsUrl = urljoin(environment.apiUrl, 'questions')
+  }
+
+   getQuestions(): Promise<void | Question[]> {
+     return this.http.get(this.questionsUrl)
+                  .toPromise()
+                  .then(response => response.json() as Question[])
+                  .catch(this.handleError)
+   }
+
+   handleError() {
+     
+   }
+
+   getQuestion() {
+
+   }
+}
