401f6447d9a1ed2eab2b031ae6af33f6bb9d95ef
thomasnrg <thomasnrgsen@gmail.com>
2018-02-17 01:34:03 -0400

    Obteniendo el detalle de una pregunta desde el Backend

--- src/app/question/question-detail.component.ts
+++ src/app/question/question-detail.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Question } from './question.model'
+import { QuestionService } from './question.service'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
-  styleUrls: ['./question-detail.component.css']
+  styleUrls: ['./question-detail.component.css'],
+  providers: [QuestionService]
 })
 
-export class QuestionDetailComponent {
-  question: Question = new Question(
-    'Esta es una nueva pregunta sobre IOS',
-    'Tengo una pregunta acerca de IOS...',
-    new Date,
-    'devicon-apple-original'
-  );
+export class QuestionDetailComponent implements OnInit{
+  question?: Question
+  loading = true
+
+  constructor(
+    private questionService: QuestionService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.questionService
+        .getQuestion(params.id)
+        .then((question: Question) => {
+          this.question = question
+        })
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe()
+  }
 }
