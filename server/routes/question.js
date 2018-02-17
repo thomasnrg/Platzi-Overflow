14a7a77699ac4704df2fc76dc1eb8d764e0ae15b
thomasnrg <thomasnrgsen@gmail.com>
2018-02-17 01:17:05 -0400

    Agregando un spinner para la carga de preguntas

--- server/routes/question.js
+++ server/routes/question.js
@@ -23,6 +23,13 @@ const questions = new Array(10).fill(question)
 // /api/questions
 app.get('/', (req, res) => res.status(200).json(questions))
 
+// /api/questions - con setTimeout
+// app.get('/', (req, res) => {
+//   setTimeout(() => {
+//     res.status(200).json(questions)
+//   }, 2000)
+// })
+
 // /api/questions/:id
 app.get('/:id', (req, res) => res.status(200).json(question))
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 const modules = [
   MatToolbarModule,
@@ -16,7 +17,8 @@ const modules = [
   MatButtonModule,
   MatListModule,
   MatGridListModule,
-  MatRadioModule
+  MatRadioModule,
+  MatProgressSpinnerModule
 ]
 
 @NgModule ({
