4ca18cb5252cf77c632e312807171e4b951d6673
thomasnrg <thomasnrgsen@gmail.com>
2018-03-03 01:13:59 -0400

    Agregando ruta para agregar pregunta a backend

--- server/routes/question.js
+++ server/routes/question.js
@@ -20,7 +20,7 @@ const question = {
 
 const questions = new Array(10).fill(question)
 
-// /api/questions
+// GET /api/questions
 app.get('/', (req, res) => res.status(200).json(questions))
 
 // /api/questions - con setTimeout
@@ -30,7 +30,27 @@ app.get('/', (req, res) => res.status(200).json(questions))
 //   }, 2000)
 // })
 
-// /api/questions/:id
-app.get('/:id', (req, res) => res.status(200).json(question))
+// GET /api/questions/:id
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  const q = questions.find(({ _id }) => _id === +id)
+  res.status(200).json(q)
+})
+
+// POST /api/questions
+app.post('/', (req, res) => {
+  const question = req.body
+  question._id = +new Date()
+  question.user = {
+    email: 'thomasnrgsen@gmail.com',
+    password: 'pyndagon',
+    firstName: 'Thomas',
+    LastName: 'Nogales'
+  }
+  question.createdAt = new Date()
+  question.answers = []
+  questions.push(question)
+  res.status(201).json(question)
+})
 
 export default app
