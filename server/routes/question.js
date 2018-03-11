c093d5f010d2df7b9933f087ee34d68cff748a60
thomasnrg <thomasnrgsen@gmail.com>
2018-03-11 00:24:16 -0400

    Agregando una ruta para crear una respuesta en el Backend

--- server/routes/question.js
+++ server/routes/question.js
@@ -2,6 +2,24 @@ import express from 'express'
 
 const app = express.Router()
 
+const currentUser = {
+  firstName: 'Thomas',
+  LastName: 'Nogales',
+  email: 'thomasnrgsen@gmail.com',
+  password: 'pyndagon'
+}
+
+function questionMiddleware(req, res, next) {
+  const { id } = req.params
+  req.question = questions.find(({ _id }) => _id === +id)
+  next()
+}
+
+function userMiddleware(req, res, next) {
+  req.user = currentUser
+  next()
+}
+
 const question = {
 
   _id: 1,
@@ -23,34 +41,30 @@ const questions = new Array(10).fill(question)
 // GET /api/questions
 app.get('/', (req, res) => res.status(200).json(questions))
 
-// /api/questions - con setTimeout
-// app.get('/', (req, res) => {
-//   setTimeout(() => {
-//     res.status(200).json(questions)
-//   }, 2000)
-// })
-
 // GET /api/questions/:id
-app.get('/:id', (req, res) => {
-  const { id } = req.params
-  const q = questions.find(({ _id }) => _id === +id)
-  res.status(200).json(q)
+app.get('/:id', questionMiddleware, (req, res) => {
+  res.status(200).json(req.question)
 })
 
 // POST /api/questions
-app.post('/', (req, res) => {
+app.post('/', userMiddleware, (req, res) => {
   const question = req.body
   question._id = +new Date()
-  question.user = {
-    email: 'thomasnrgsen@gmail.com',
-    password: 'pyndagon',
-    firstName: 'Thomas',
-    LastName: 'Nogales'
-  }
+  question.user = req.user
   question.createdAt = new Date()
   question.answers = []
   questions.push(question)
   res.status(201).json(question)
+})
+
+// POST /api/questions/:id
+app.post('/:id/answers', questionMiddleware, userMiddleware, (req, res) => {
+  const answer = req.body
+  const q = req.question
+  answer.createdAt = new Date()
+  answer.user = req.user
+  q.answers.push(answer)
+  res.status(201).json(answer)
 })
 
 export default app
